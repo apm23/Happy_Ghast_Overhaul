3901c4c2482cd52cd4e5a64dc06f3d64c97ac0fd
apm23 <80390905+apm23@users.noreply.github.com>
2026-09-05 01:11:11 +0900

    Fix Fabric render-state key declaration

--- src/client/java/com/apm23/happyghastoverhaul/client/render/MilitaryHarnessRenderData.java
+++ src/client/java/com/apm23/happyghastoverhaul/client/render/MilitaryHarnessRenderData.java
@@ -3,7 +3,9 @@ package com.apm23.happyghastoverhaul.client.render;
 import net.fabricmc.fabric.api.client.rendering.v1.RenderStateDataKey;
 
 public final class MilitaryHarnessRenderData {
-    public static final RenderStateDataKey<Boolean> EQUIPPED = RenderStateDataKey.create(() -> Boolean.FALSE);
+    public static final RenderStateDataKey<Boolean> EQUIPPED = RenderStateDataKey.create(
+            () -> "happy_ghast_overhaul:military_harness_equipped"
+    );
 
     private MilitaryHarnessRenderData() {
     }
